302bc1dcbaa3a6886097444d8ded7f7bc9995578
Lenon <jlenon@deloitte.com.au>
2017-12-07 11:26:09 +1030

    Initial commit

---
+++ Electricity Bill/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace Electricty_Bill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtSavings = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.txtAmountNoSolar = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.Label();
+            this.txtDateFrom = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTotalDays = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDateTo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnProcess);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtFile);
+            this.groupBox1.Controls.Add(this.btnSelect);
+            this.groupBox1.Location = new System.Drawing.Point(29, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(444, 160);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(141, 85);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(146, 52);
+            this.btnProcess.TabIndex = 7;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "File";
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(64, 26);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(286, 20);
+            this.txtFile.TabIndex = 5;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(356, 24);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 4;
+            this.btnSelect.Text = "...";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtSavings);
+            this.groupBox2.Controls.Add(this.label13);
+            this.groupBox2.Controls.Add(this.txtAmountNoSolar);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.txtAmount);
+            this.groupBox2.Controls.Add(this.txtDateFrom);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.txtTotalDays);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtDateTo);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(29, 205);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(444, 221);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // txtSavings
+            // 
+            this.txtSavings.AutoSize = true;
+            this.txtSavings.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSavings.Location = new System.Drawing.Point(197, 173);
+            this.txtSavings.Name = "txtSavings";
+            this.txtSavings.Size = new System.Drawing.Size(70, 24);
+            this.txtSavings.TabIndex = 26;
+            this.txtSavings.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(17, 173);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(83, 24);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Savings";
+            // 
+            // txtAmountNoSolar
+            // 
+            this.txtAmountNoSolar.AutoSize = true;
+            this.txtAmountNoSolar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmountNoSolar.Location = new System.Drawing.Point(197, 135);
+            this.txtAmountNoSolar.Name = "txtAmountNoSolar";
+            this.txtAmountNoSolar.Size = new System.Drawing.Size(70, 24);
+            this.txtAmountNoSolar.TabIndex = 24;
+            this.txtAmountNoSolar.Text = "label10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(17, 135);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(177, 24);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Amount (no solar)";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.AutoSize = true;
+            this.txtAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmount.Location = new System.Drawing.Point(197, 99);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(60, 24);
+            this.txtAmount.TabIndex = 22;
+            this.txtAmount.Text = "label9";
+            // 
+            // txtDateFrom
+            // 
+            this.txtDateFrom.AutoSize = true;
+            this.txtDateFrom.Location = new System.Drawing.Point(86, 23);
+            this.txtDateFrom.Name = "txtDateFrom";
+            this.txtDateFrom.Size = new System.Drawing.Size(35, 13);
+            this.txtDateFrom.TabIndex = 21;
+            this.txtDateFrom.Text = "label8";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(17, 99);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 24);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Amount";
+            // 
+            // txtTotalDays
+            // 
+            this.txtTotalDays.AutoSize = true;
+            this.txtTotalDays.Location = new System.Drawing.Point(86, 73);
+            this.txtTotalDays.Name = "txtTotalDays";
+            this.txtTotalDays.Size = new System.Drawing.Size(35, 13);
+            this.txtTotalDays.TabIndex = 19;
+            this.txtTotalDays.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Total Days";
+            // 
+            // txtDateTo
+            // 
+            this.txtDateTo.AutoSize = true;
+            this.txtDateTo.Location = new System.Drawing.Point(86, 47);
+            this.txtDateTo.Name = "txtDateTo";
+            this.txtDateTo.Size = new System.Drawing.Size(35, 13);
+            this.txtDateTo.TabIndex = 17;
+            this.txtDateTo.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Date To";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Date From";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 458);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Electricty Bill Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label txtSavings;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label txtAmountNoSolar;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label txtAmount;
+        private System.Windows.Forms.Label txtDateFrom;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label txtTotalDays;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label txtDateTo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Electricity Bill/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpreadsheetLight;
+
+namespace Electricty_Bill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            txtFile.Text = @"c:\Users\jlenon\Downloads\Jimmy_Lenon_09092017-06122017.xlsx";
+
+
+            txtDateFrom.Text = "";
+            txtDateTo.Text = "";
+            txtTotalDays.Text = "";
+
+            txtAmount.Text = "$0.00";
+            txtAmountNoSolar.Text = "$0.00";
+            txtSavings.Text = "$0.00";
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            //Load the file
+            SLDocument ss = new SLDocument(txtFile.Text, "Sheet1");
+
+            //read spreadsheet
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Now;
+            
+            bool eof = false;
+            int currentCell = 3;
+
+            double energyFromGrid = 0;
+            double energyToGrid = 0;
+            double consumption = 0;
+
+            while (!eof)
+            {
+                DateTime cellValueDateTime = ss.GetCellValueAsDateTime("A" + currentCell);
+                string cellValue = ss.GetCellValueAsString("A" + currentCell);
+
+                if (String.IsNullOrEmpty(cellValue))
+                {
+                    eof = true;
+                }
+                else
+                {
+                    consumption += ss.GetCellValueAsDouble("C" + currentCell);
+                    energyFromGrid += ss.GetCellValueAsDouble("D" + currentCell);
+                    energyToGrid += ss.GetCellValueAsDouble("E" + currentCell);
+
+                    if (currentCell == 3)
+                    {
+                        startDate = cellValueDateTime;
+                    }
+                    else
+                    {
+                        endDate = cellValueDateTime;
+                    }
+                }
+                
+                currentCell++;
+            }
+
+            txtDateFrom.Text = startDate.ToShortDateString();
+            txtDateTo.Text = endDate.ToShortDateString();
+            int totalDays = (int) endDate.Subtract(startDate).TotalDays;
+            txtTotalDays.Text = totalDays.ToString();
+
+            double amountSolar = GetBillAmount(energyFromGrid, energyToGrid, 0.4, 0.17, totalDays, 0.6788, 0.8213, 0.22);
+            double amountNoSolar = GetBillAmount(consumption, 0, 0.4, 0.17, totalDays, 0.6788, 0.8213, 0.22);
+            double savings = amountNoSolar - amountSolar;
+
+            txtAmount.Text = amountSolar.ToString("C2");
+            txtAmountNoSolar.Text = amountNoSolar.ToString("C2");
+            txtSavings.Text = savings.ToString("C2");
+
+        }
+
+        private double GetBillAmount(double energyIn, double energyOut, double energyInCost, double energyOutCost, int totalDays, double extraPerDay, double supplyChargesPerDay, double percentageSavings)
+        {
+            //Convert fro Wh to Kwh
+            energyIn = energyIn / 1000;
+            energyOut = energyOut / 1000;
+
+            //Work out our total cost of energy
+            double totalEnergyCost = energyIn * energyInCost - energyOut * energyOutCost;
+
+            //Add on any extra costs per day such as off peak power
+            totalEnergyCost = totalEnergyCost + (totalDays * extraPerDay);
+
+            //Add on supply charges
+            totalEnergyCost = totalEnergyCost + (totalDays * supplyChargesPerDay);
+
+            //Subtract any percentage savings
+            totalEnergyCost = totalEnergyCost - (totalEnergyCost * percentageSavings);
+
+            //Apply 10% GST
+            totalEnergyCost = totalEnergyCost * 1.1;
+
+            return totalEnergyCost;
+        }
+    }
+}
